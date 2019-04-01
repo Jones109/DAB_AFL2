ee5d3b66876b6fdb1dd8022510f394898c057850
Jonas <au544262@post.au.dk>
2019-04-01 11:45:03 +0200

    Created Enrolled

---
+++ DAB_AFL2/Models/Enrolled.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace DAB_AFL2.Models
+{
+    class Enrolled
+    {
+        public int CourseId;
+        public int StudentId;
+        public string Status;
+    }
+}
