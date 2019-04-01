1f5b62ba006c1f51fdecc9d4a8a04e96e8898532
JonathanFL <j.lydemann@gmail.com>
2019-04-01 12:41:45 +0200

    Update Folder.cs

--- DAB_AFL2/Models/CourseContent/Folder.cs
+++ DAB_AFL2/Models/CourseContent/Folder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace DAB_AFL2.Models.CourseContent
@@ -8,7 +9,10 @@ namespace DAB_AFL2.Models.CourseContent
     public class Folder
     {
         [Key]
-        public int FolderId { get; set; }
+        public string Name { get; set; }
 
+        public string Parent { get; set; }
+
+        public int Course_FK { get; set; }
     }
 }
