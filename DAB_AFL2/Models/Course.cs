44b0db45e1cbb78b12ef7fd51119400193e2c984
JonathanFL <j.lydemann@gmail.com>
2019-04-01 11:16:54 +0200

    SKal have nogle teachers

---
+++ DAB_AFL2/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAB_AFL2.Models
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        [Key]
+        public int CourseId { get; set; }
+    }
+}
