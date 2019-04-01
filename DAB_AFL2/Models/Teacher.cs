29ef118c86c5c92b04f65ccc2e7789d11b580944
Mathias Nielsen <mathiasnielsen0@gmail.com>
2019-04-01 11:18:24 +0200

    Teacher

---
+++ DAB_AFL2/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DAB_AFL2.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int StudentId { get; set; }
+        [MaxLength(Int16.MaxValue)]
+        public short Birthday { get; set; }
+        [MaxLength(128)]
+        public string Name { get; set; }
+        
+
+    }
+}
