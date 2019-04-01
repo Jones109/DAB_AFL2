ed89f769d5fa976256b138eece844ecdf183fc02
Jonas <au544262@post.au.dk>
2019-04-01 11:38:22 +0200

    Created GroupStudents

---
+++ DAB_AFL2/Models/GroupStudents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAB_AFL2.Models
+{
+    class GroupStudents
+    {
+        public int StudentId;
+        public int GroupId;
+    }
+}
